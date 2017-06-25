26127bab4230d2a03f32b57d88ae0de3b4665d4e
sunrun93 <wangjian9302@163.com>
2017-06-25 22:47:57 +0800

    给图片重新分配位置

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');//引入CSS方式
+require('styles/App.less');//引入CSS方式
+let ReactDOM = require('react-dom');
 
 import React from 'react';
 //获取图片相关的数据
@@ -14,10 +15,21 @@ imageDatas  = (function genImageURL(imageDataArr) {
   return imageDataArr;
 })(imageDatas);
 
+//获取区间内的随机值
+function getRangeRandom(low,high){
+  return Math.ceil(Math.random()*(high-low)+low);
+}
+
+
 let ImgFigure = React.createClass({
   render:function(){
+    let styleObj={};
+    //如果props属性中制定了这张图片的位置，则使用
+    if(this.props.arrange.pos){
+      styleObj = this.props.arrange.pos;
+    }
     return(
-      <figure className="img-figure">
+      <figure className="img-figure" style={styleObj}>
         <img src={this.props.data.imageURL} alt={this.props.data.title}/>
         <figcaption>
           <h2 className="img-title">{this.props.data.title}</h2>
@@ -27,29 +39,167 @@ let ImgFigure = React.createClass({
   }
 })
 
+var AppComponent = React.createClass({
+  Constant: {//初始化各分区的取值都为0
+    centerPos: {
+      left: 0,
+      right: 0
+    },
+    hPosRange: {//水平方向取值范围
+      leftSecX: [0, 0],//左分区
+      rightSecX: [0, 0],//右分区
+      y: [0, 0]
+    },
+    vPosRange: {//垂直方向取值范围
+      x: [0, 0],
+      topY: [0, 0]
+    }
+  },
+  /*
+  *重新布局所有图片
+  *@param centerIndex指定居中排布哪个图片
+   */
+  rearrange:function(centerIndex){
+    let imgsArrangeArr = this.state.imgsArrangeArr,
+    Constant = this.Constant,
+    centerPos = Constant.centerPos,
+    hPosRange = Constant.hPosRange,
+    vPosRange = Constant.vPosRange,
+    //左右两侧区域x,y的取值范围
+    hPosRangeLeftSecX = hPosRange.leftSecX,
+    hPosRangeRightSecX = hPosRange.rightSecX,
+    hPosRangeY = hPosRange.y,
+    //上侧区域x,y的取值范围
+    vPosRangeTopY = vPosRange.topY,
+    vPosRangeX = vPosRange.x,
 
+    //用以存储上侧图片的状态信息
+    imgsArrangeTopArr = [];
+    let topImgNum = Math.ceil(Math.random()*2);//取一个或者不取
+    let topImgSpliceIndex = 0;//上侧图片是数组中的哪一个，初始化为0
 
-class AppComponent extends React.Component {
+    let imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex,1);//中心图片的状态信息
+
+    //首先居中centerIndex的图片
+    imgsArrangeCenterArr[0].pos = centerPos;
+
+    //取出要布局上侧图片索引  取上侧图片的状态信息
+    topImgSpliceIndex = Math.ceil(Math.random(imgsArrangeArr.length-topImgNum));
+    imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex,topImgNum);
+    // 布局上侧的图片
+    imgsArrangeTopArr.forEach(function(value,index){
+      imgsArrangeTopArr[index].pos = {
+        top: getRangeRandom(vPosRangeTopY[0],vPosRangeTopY[1]),
+        left: getRangeRandom(vPosRangeX[0],vPosRangeX[1])
+      }
+    })
+
+    //布局左右两侧的图片
+    for(let i=0,j = imgsArrangeArr.length,k=j/2;i<j;i++){
+      let hPosRangeLorRX = null;
+      //前半部分布局左边，右半部分布局右边
+      if(i<k){
+        hPosRangeLorRX = hPosRangeLeftSecX;
+      }else{
+        hPosRangeLorRX = hPosRangeRightSecX;
+      }
+      imgsArrangeArr[i].pos={
+        top:getRangeRandom(hPosRangeY[0],hPosRangeY[1]),
+        left:getRangeRandom(hPosRangeLorRX[0],hPosRangeLorRX[1])
+      }
+    }
+    //把top元素插回到原来的数组中
+    if(imgsArrangeTopArr &&imgsArrangeTopArr[0]){
+      imgsArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr[0]);
+    }
+    //把中间区域的图片元素插回原来的数组
+    imgsArrangeArr.splice(centerIndex,0,imgsArrangeCenterArr[0]);
+
+    //设置state触发component的重新渲染
+    this.setState({
+      imgsArrangeArr:imgsArrangeArr
+    });
+  },
+  getInitialState:function(){
+    return{
+      imgsArrangeArr:[
+        
+      ]
+    }
+  },
+
+  
+  //图片加载后，为每个图片计算其位置
+  componentDidMount:function () {//加载完成后的构造函数
+    //拿到舞台大小
+    let stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+    stageW = stageDOM.scrollWidth;
+    let stageH = stageDOM.scrollHeight;
+    let halfStageW = Math.ceil(stageW/2);
+    let halfStageH = Math.ceil(stageH/2);
+
+    let imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0);
+    let imgW=imgFigureDOM.scrollWidth;
+    let imgH=imgFigureDOM.scrollHeight;
+    let halfImgW=Math.ceil(imgW/2);
+    let halfImgH=Math.ceil(imgH/2);
+    //计算中心图片位置点
+    this.Constant.centerPos = {
+      left:halfStageW-halfImgW,
+      top:halfStageH-halfImgH
+    }
+
+    //计算左两侧区域的x取值范围
+    this.Constant.hPosRange.leftSecX[0] =-halfImgW;
+    this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW*3;
+    //计算右侧区域x的取值范围
+    this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+    this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+    //计算左右两侧区域Y的取值范围
+    this.Constant.hPosRange.y[0] = -halfImgH;
+    this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+    //计算上侧区域y的取值范围
+    this.Constant.vPosRange.topY[0] = -halfImgH;
+    this.Constant.vPosRange.topY[1] = halfStageH - halfImgH*3;
+    //计算上侧区域x的取值范围
+    this.Constant.vPosRange.x[0] = halfImgW-imgW;
+    this.Constant.vPosRange.x[1] = halfImgW;
+
+    this.rearrange(0);
+
+  },
   render() {
     let controllerUnits = [];
     let imgFigures = [];
-    imageDatas.forEach((value)=>{
-        imgFigures.push(<ImgFigure data={value}/>);
-    });
+
+    imageDatas.forEach(function(value,index){
+
+    if(!this.state.imgsArrangeArr[index]){
+      this.state.imgsArrangeArr[index] = {
+        pos:{
+          left:0,
+          top:0
+        }
+      }
+    }
+    
+      imgFigures.push(<ImgFigure key={index} data={value} ref={'imgFigure'+index }
+      arrange={this.state.imgsArrangeArr[index]}/>);//通过arrange将图片的状态信息传递给imgsFigure
+    }.bind(this));//用箭头函数bind方法报错
+
     return (
-      <section className="stage">
-          <section className="image-sec">
-              {imgFigures}
-          </section>
-          <nav className="controller-nav">
-              {controllerUnits}
-          </nav>
+      <section className="stage" ref="stage">
+        <section className="image-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+          {controllerUnits}
+        </nav>
       </section>
     );
   }
-}
-
-AppComponent.defaultProps = {
-};
-
-export default AppComponent;
+  })
+ 
+ReactDOM.render(<AppComponent/>,document.getElementById('app'));//注意reactDOM.render取代原来的方法
+module.exports = AppComponent;
